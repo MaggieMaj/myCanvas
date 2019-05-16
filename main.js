b78b3b1da510507ce949e44f427b582a679d7fea
Maggie <858123145@qq.com>
2019-05-16 13:53:18 +0800

    添加下载

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var yyy = document.getElementById('xxx'); //获取一个画布赋予yyy
 var context = yyy.getContext('2d'); //获取yyy二次元上下文
+var lineWidth = 5   //默认线宽为5px；
 
 autoSetCanvasSize(yyy);
 
@@ -13,21 +14,78 @@ eraser.onclick = function() {
 
 //老司机做法
 var eraserEnabled = false; //默认不启用橡皮擦
-eraser.onclick = function() {
-  eraserEnabled=true;     //橡皮擦启用
-  actions.className='actions x';
-};
-brush.onclick=function(){
-  eraserEnabled=false;     //画笔启用
-  actions.className='actions';
-};
+pen.onclick = function(){
+  eraserEnabled = false
+  pen.classList.add('active')
+  eraser.classList.remove('active')
+}
+eraser.onclick = function(){
+  eraserEnabled = true
+  eraser.classList.add('active')
+  pen.classList.remove('active')
+}
+
+clear.onclick = function(){
+  context.clearRect(0,0,yyy.width,yyy.height);
+}
+
+download.onclick = function(){
+  var url = yyy.toDataURL("image/png")
+  //console.log(url)
+  var a =document.createElement('a')//生成a标签
+  document.body.appendChild(a)//把a放到页面里面
+  a.href = url //href用于外部自由的引用
+  a.download = '我的画儿'//a标签加个download
+  a.target = '_blank'//开一个新页面
+  a.click()//调用a的click，a被点击一下 
+}
+
+black.onclick = function(){
+  context.fillstyle = 'black'
+  context.strokeStyle = 'black'
+  black.classList.add('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+  yellow.classList.remove('active')
+}
+
+red.onclick = function(){
+  context.fillstyle = 'red'
+  context.strokeStyle = 'red'
+  red.classList.add('active')
+  black.classList.remove('active')
+  green.classList.remove('active')
+  yellow.classList.remove('active')
+}
+green.onclick = function(){
+  context.fillstyle = 'green'
+  context.strokeStyle = 'green'
+  green.classList.add('active')
+  black.classList.remove('active')
+  red.classList.remove('active')
+  yellow.classList.remove('active')
+}
+yellow.onclick = function(){
+  context.fillstyle = 'yellow'
+  context.strokeStyle = 'yellow'
+  yellow.classList.add('active')
+  black.classList.remove('active')
+  red.classList.remove('active')
+  green.classList.remove('active')
+}
+
+thin.onclick = function(){
+  lineWidth=5;
+}
+thick.onclick = function(){
+  lineWidth=10;
+}
 
 /*********/
 function drawLine(x1, y1, x2, y2) { //旧点与新点连线
   context.beginPath();
-  context.strokeStyle = 'black';
   context.moveTo(x1, y1); //起点坐标
-  context.lineWidth = 5;
+  context.lineWidth = lineWidth;
   context.lineTo(x2, y2); //终点坐标
   context.stroke();
   context.closePath();
